526e8dea9570428baffba2b62c36bd2d0db458da
Naledi Sandamela <56083410+seymourpublic@users.noreply.github.com>
2025-04-25 18:34:50 +0200

    Update Apollo client configuration to use production API URL and remove social share buttons from PostDetail component

--- apollo-client.js
+++ apollo-client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/graphql/v1',
+  uri: process.env.NEXT_PUBLIC_API_URL || 'https://versablog-backend.onrender.com/graphql/v1',
 });
 
 const authLink = setContext((_, { headers }) => {
